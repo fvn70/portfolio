73271542e5fc3ff5772792591091aa117b328f70
fvn70 <fvn_reg@mail.ru>
2022-04-09 17:46:58 +0700

    Stage 5/5: Adaptability and сross-browser compatibility

--- Portfolio/task/src/code.js
+++ Portfolio/task/src/code.js
@@ -1,10 +1,29 @@
-let modal1 = document.querySelector("#modal1");
-let modal2 = document.querySelector("#modal2");
+const modal1 = document.querySelector("#modal1");
+const modal2 = document.querySelector("#modal2");
 // Get the button that opens and close the modal
-let openBtn1 = document.querySelector("#oBtn1");
-let openBtn2 = document.querySelector("#oBtn2");
-let closeBtn1 = document.querySelector("#cBtn1");
-let closeBtn2 = document.querySelector("#cBtn2");
+const openBtn1 = document.querySelector("#oBtn1");
+const openBtn2 = document.querySelector("#oBtn2");
+const closeBtn1 = document.querySelector("#cBtn1");
+const closeBtn2 = document.querySelector("#cBtn2");
+
+const burger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click',() => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade .5s easy`;
+        }
+    });
+    // Burger animation
+    burger.classList.toggle('toggle');
+});
 
 openBtn1.onclick = () => {
     modal1.style.display = "block";
@@ -31,3 +50,4 @@ window.onclick = (e) => {
         modal2.style.display = "none";
     }
 }
+
